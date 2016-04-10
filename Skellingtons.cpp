34e99cd3358230c28994b89401603ed3dd546629
Jordan Carroll <jfindlayc@gmail.com>
2016-04-10 15:03:54 -0400

    misc structure edits

--- Skellingtons.cpp
+++ Skellingtons.cpp
@@ -3,23 +3,40 @@
 // A SKELETON GAME
 // by Jordan Carroll and Kevin Smith
 
+using namespace std;
+
+// Functions
+
+	void startgame();
+	void endgame();
+
 
 // The Player class
 class Player
 {
-int health;
+private:
+	int health;
 
+public:
+	int position[1][1];
+	void thwack();
+};
 
+// The enemy class
+class Enemy
+{
 
+public:
+	int position[1][1];
+	void spawn();
+	void die();
 };
 
 
 
 int main() 
 {
-
-
-
+	
 
 	return 0;
 }
