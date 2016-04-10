e425c87d9349e6e95ea7406f2553f12e1da75fbb
Jordan Carroll <jfindlayc@gmail.com>
2016-04-10 17:20:08 -0400

    Added basic print protyping for testing position of player

--- Skellingtons.cpp
+++ Skellingtons.cpp
@@ -4,6 +4,7 @@
 // by Jordan Carroll and Kevin Smith
 
 #include <iostream>
+
 using namespace std;
 
 
@@ -11,16 +12,19 @@ using namespace std;
 class Player
 {
 private:
-	int health;
-	int xpos, ypos;
+	int xpos, ypos, health;
 
 public:
 	void thwack();
 
 	int getxpos()
-		{return xpos};
+		{return xpos;}
 	int getypos()
-		{return ypos};
+		{return ypos;}
+
+// Constructor prototype
+	Player(int, int, int);
+//	~Player();
 };
 
 // The enemy class
@@ -37,9 +41,12 @@ public:
 	void setPosition(int, int);
 
 	int getxpos()
-		{return xpos};
+		{return xpos;}
 	int getypos()
-		{return ypos};
+		{return ypos;}
+// Constructor prototype
+	Enemy(int, int);
+//	~Enemy();
 };
 
 
@@ -52,10 +59,8 @@ public:
 
 	void startgame();
 	void endgame();
-
-
-
-
+	
+	void printboard(int px, int py);
 
 
 
@@ -63,7 +68,85 @@ public:
 
 int main() 
 {
-	
+	Player Player1(3, 3, 3);
+	printboard(Player1.getxpos(), Player1.getypos());
+int a;
+int b;
+
+
+cout << "Move to which x?" ;
+cin >> a;
+cout << "Move to which y?";
+cin >> b;
+
+printboard(a, b);
+
+
 
 	return 0;
 }
+
+// Functions
+
+
+// Constructor Definition blocks
+	Player::Player(int xpos, int ypos, int health)
+	{	
+		this -> xpos = xpos;
+		this -> ypos = ypos;
+		this -> health = health;
+	}
+
+
+	Enemy::Enemy(int xpos, int ypos) 
+	{
+		this -> xpos = xpos;
+		this -> ypos = ypos;
+	}
+
+
+
+// Board printing function
+
+void printboard(int px, int py)
+{
+	int i, j;
+	int playerxpos = px;
+	int playerypos = py;
+	for ( i = 5; i > 0; i--) {
+		cout << "XXXXXXXXXXX" << endl;
+		for ( j = 1; j < 6; j ++) {
+			cout << "X";
+			if (playerxpos == i && playerypos == j) {
+				cout << "P";
+			}
+			else
+				{cout << " ";}
+		}
+		cout << "X" << endl;	
+	}
+
+
+	cout << "XXXXXXXXXXX" << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
